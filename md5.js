6b259368072be55c4e344dd0676caf6cb864e99a
Triple3T <triple@triple-lab.com>
2025-08-08 11:31:35 +0900

    Create md5.js

---
+++ md5.js
@@ -0,0 +1,105 @@
+const s = [
+  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,
+];
+const k = [
+  0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+  0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+  0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+  0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+  0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+  0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+  0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+  0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
+];
+
+/**
+ * calculate MD5 hash from given Uint8Array.
+ * @param {Uint8Array} arr data to calculate MD5 hash.
+ */
+const getMd5 = (arr) => {
+  // 1. pad data with 1, 0 and length. result: `r`
+  const len = arr.length;
+  const lengthForMultOf512 = Math.ceil((len + 9) / 64) * 64;
+  const r = new Uint8Array(lengthForMultOf512).fill(0);
+  r.set(arr);
+  r[len] = 128;
+  let lenBit = BigInt(len) * 8n;
+  for (let i = 0; i < 8; i++) {
+    r[lengthForMultOf512 - 8 + i] = Number(lenBit % 256n);
+    lenBit /= 256n;
+  }
+
+  // 2. initialize buffer and process
+  let a0 = 0x67452301;
+  let b0 = 0xefcdab89;
+  let c0 = 0x98badcfe;
+  let d0 = 0x10325476;
+  const chunkCount = lengthForMultOf512 / 64;
+  for (let i = 0; i < chunkCount; i++) {
+    const m = [];
+    for (let j = 0; j < 16; j++) {
+      const offset = i * 64 + j * 4;
+      m[j] = r[offset] + r[offset + 1] * 256 + r[offset + 2] * 65536 + r[offset + 3] * 16777216;
+    }
+    let a = a0;
+    let b = b0;
+    let c = c0;
+    let d = d0;
+    for (let j = 0; j < 64; j++) {
+      let f;
+      let g;
+      if (j < 16) {
+        f = ((b & c) | (~b & d)) >>> 0;
+        g = j;
+      } else if (j < 32) {
+        f = ((d & b) | (~d & c)) >>> 0;
+        g = (5 * j + 1) % 16;
+      } else if (j < 48) {
+        f = (b ^ c ^ d) >>> 0;
+        g = (3 * j + 5) % 16;
+      } else {
+        f = (c ^ (b | ~d)) >>> 0;
+        g = (7 * j) % 16;
+      }
+      f = (f + a + k[j] + m[g]) >>> 0;
+      a = d;
+      d = c;
+      c = b;
+      b = b + (((f << s[j]) | (f >>> (32 - s[j]))) >>> 0);
+    }
+    a0 = (a0 + a) >>> 0;
+    b0 = (b0 + b) >>> 0;
+    c0 = (c0 + c) >>> 0;
+    d0 = (d0 + d) >>> 0;
+  }
+
+  // 3. concat to return final result
+  return [a0, b0, c0, d0].reduce((acc, num) => {
+    for (let i = 0; i < 4; i++) {
+      acc += (num % 256).toString(16).padStart(2, '0');
+      num = Math.floor(num / 256);
+    }
+    return acc;
+  }, '');
+  // or use
+  // return [a0, b0, c0, d0].reduce((acc, num, idx) => {
+  //   for (let i = 0; i < 4; i++) {
+  //     acc[idx * 4 + i] = num % 256;
+  //     num = Math.floor(num / 256);
+  //   }
+  //   return acc;
+  // }, new Uint8Array(16));
+};
+
+/**
+ * calculate MD5 hash from given string.
+ * @param {string} str data to calculate MD5 hash.
+ */
+const getMd5FromString = (str) => {
+  const encoder = new TextEncoder();
+  const uint8array = encoder.encode(str);
+  return getMd5(uint8array);
+};
